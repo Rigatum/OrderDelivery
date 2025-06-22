2b7457dec1d4c0ef1e0f25423b802e1f16412d05
Тагир Мельников <tagir-CSGO@mail.ru>
2025-06-22 16:44:25 +0700

    change controller for route

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ app.ApplyMigrations();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Order/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -31,7 +31,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Order}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
